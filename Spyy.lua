0faf09c5ec59255aed5bbffe6ee43e945eee382a
T1k <razdobudko13@gmail.com>
2025-08-03 00:46:18 +0300

    Create Spyy.lua

---
+++ Spyy.lua
@@ -0,0 +1,29 @@
+local source = [[
+	loadstring(game:HttpGetAsync("https://github.com/richie0866/remote-spy/releases/latest/download/RemoteSpy.lua"))()
+]]
+
+-- etc
+
+local env = getfenv()
+
+-- Выполняем оригинальный loadstring
+local result = loadstring(source)
+if result then
+	local uiRoot = result()
+
+	-- Находим корневой GUI (если это ScreenGui, например)
+	if typeof(uiRoot) == "Instance" and uiRoot:IsA("ScreenGui") then
+		-- Центруем
+		for _, child in ipairs(uiRoot:GetChildren()) do
+			if child:IsA("Frame") or child:IsA("CanvasGroup") then
+				child.AnchorPoint = Vector2.new(0.5, 0.5)
+				child.Position = UDim2.new(0.5, 0, 0.5, 0)
+			end
+		end
+
+		-- Добавляем масштаб
+		local scale = Instance.new("UIScale")
+		scale.Scale = 0.5
+		scale.Parent = uiRoot
+	end
+end
